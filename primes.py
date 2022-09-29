5c65da7ed1f912920efa22c368ba9e483cd50ea8
Harry Sharma <harshit.sharma@kcl.ac.uk>
2022-09-29 14:37:42 +0100

    Task complete

--- primes.py
+++ primes.py
@@ -1,6 +1,25 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+"""Websites used for syntax:
+https://www.w3schools.com/python/python_for_loops.asp  
+https://www.w3schools.com/python/python_lists_methods.asp  
+https://www.w3schools.com/python/python_conditions.asp
+https://www.w3schools.com/python/python_while_loops.asp
+"""
 
+   
 def primes(number_of_primes):
-    list = []
+    list=[]
+    count=2
+    while number_of_primes!=0:
+        for i in range(2,count):
+            if(count%i==0):
+                break
+        else:
+            list.append(count)
+            number_of_primes-=1
+        count+=1
     return list
+
+
+print(primes(10))
